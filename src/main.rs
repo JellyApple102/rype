ef56a275a61604f189f4086b53c37b3607ca361e
JellyApple102 <jcarryer@outlook.com>
2021-10-20 17:11:26 -0400

    game/timer render functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::io;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
+use tui::{Frame, Terminal};
+use tui::backend::{Backend, CrosstermBackend};
 use tui::widgets::{Block, BorderType, Borders, Paragraph};
-use tui::layout::{Alignment, Constraint, Direction, Layout};
+use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
+use tui::style::{Style, Color};
 use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 use crossterm::event::{self, Event as CEvent, KeyCode};
 use std::sync::mpsc;
@@ -12,6 +13,18 @@ use std::time::{Duration, Instant};
 enum Event<I> {
     Input(I),
     Tick
+}
+
+enum FocusedWindow {
+    GameOptions,
+    TimerOptions,
+    Game
+}
+
+enum GameState {
+    Pre,
+    During,
+    Post
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -50,24 +63,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
+    let focused_window = FocusedWindow::Game;
+    let game_state = GameState::Pre;
 
     // draw loop
     loop {
         terminal.draw(|f| {
-            let main_chunk = Block::default()
+            let title_chunk = Block::default()
                 .title("rype")
                 .borders(Borders::ALL)
                 .border_type(BorderType::Rounded);
-            f.render_widget(main_chunk, f.size());
+            f.render_widget(title_chunk, f.size());
 
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 .margin(1)
                 .constraints(
                     [
-                    Constraint::Length(3),
-                    Constraint::Min(0),
-                    Constraint::Length(3)
+                    Constraint::Length(3), // header chunk
+                    Constraint::Min(0),    // game chunks
+                    Constraint::Length(3)  // footer chunk
                     ].as_ref()
                 )
                 .split(f.size());
@@ -95,19 +110,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 )
                 .split(game_chunks[0]);
 
-            let header = Paragraph::new("Header")
-                .block(Block::default().borders(Borders::ALL).border_type(BorderType::Rounded))
-                .alignment(Alignment::Center);
-            f.render_widget(header, chunks[0]);
+            let header_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(
+                    [
+                    Constraint::Percentage(50),
+                    Constraint::Percentage(50)
+                    ].as_ref()
+                )
+                .split(chunks[0]);
+
+            match focused_window {
+                FocusedWindow::GameOptions => {
+                    render_game_options(f, header_chunks[0], true);
+                    render_timer_options(f, header_chunks[1], false);
+                },
+                FocusedWindow::TimerOptions => {
+                    render_game_options(f, header_chunks[0], false);
+                    render_timer_options(f, header_chunks[1], true);
+                },
+                FocusedWindow::Game => {
+                    render_game_options(f, header_chunks[0], false);
+                    render_timer_options(f, header_chunks[1], false);
+                }
+            }
+
+            let typing_section = Paragraph::new("type here")
+                // .block(Block::default().borders(Borders::ALL).border_type(BorderType::Rounded))
+                .alignment(Alignment::Left);
+            f.render_widget(typing_section, game_chunks[1]);
 
             let timer = Paragraph::new("timer here")
                 .alignment(Alignment::Left);
             f.render_widget(timer, timer_chunks[1]);
-
-            let typing_section = Paragraph::new("type here")
-                .block(Block::default().borders(Borders::ALL).border_type(BorderType::Rounded))
-                .alignment(Alignment::Left);
-            f.render_widget(typing_section, game_chunks[1]);
 
             let footer = Paragraph::new("Footer")
                 .block(Block::default().borders(Borders::ALL).border_type(BorderType::Rounded))
@@ -130,4 +165,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     Ok(())
+}
+
+fn render_game_options<B: Backend>(f: &mut Frame<B>, area: Rect, focused: bool) {
+    if focused {
+        let game_options_tabs = Paragraph::new("game options here")
+            .block(Block::default()
+                   .borders(Borders::LEFT | Borders::TOP | Borders::BOTTOM)
+                   .border_type(BorderType::Rounded)
+                   .border_style(Style::default().fg(Color::Red)))
+            .alignment(Alignment::Left);
+
+        f.render_widget(game_options_tabs, area);
+    } else {
+        let game_options_tabs = Paragraph::new("game options here")
+            .block(Block::default()
+                   .borders(Borders::LEFT | Borders::TOP | Borders::BOTTOM)
+                   .border_type(BorderType::Rounded))
+            .alignment(Alignment::Left);
+
+        f.render_widget(game_options_tabs, area);
+    }
+}
+
+fn render_timer_options<B: Backend>(f: &mut Frame<B>, area: Rect, focused: bool) {
+    if focused {
+        let timer_options_tabs = Paragraph::new("timer options here")
+            .block(Block::default()
+                   .borders(Borders::RIGHT | Borders::TOP | Borders::BOTTOM)
+                   .border_type(BorderType::Rounded)
+                   .border_style(Style::default().fg(Color::Red)))
+            .alignment(Alignment::Right);
+
+        f.render_widget(timer_options_tabs, area);
+    } else {
+        let timer_options_tabs = Paragraph::new("timer options here")
+            .block(Block::default()
+                   .borders(Borders::RIGHT | Borders::TOP | Borders::BOTTOM)
+                   .border_type(BorderType::Rounded))
+            .alignment(Alignment::Right);
+
+        f.render_widget(timer_options_tabs, area);
+    }
 }
